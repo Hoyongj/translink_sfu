1ee3e68736fada08afc4be8772fafc8c01355cf0
Hoyong Jung <hoyong1121@icloud.com>
2022-10-15 17:29:25 -0700

    WORKING practically, but sometimes return nega min

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ van_to_bur = True
 
 def change_direction():
     global van_to_bur
+    label_bur.place_forget()
+    label_van.place_forget()
     if van_to_bur:
         van_to_bur = False
         btn_dir['text'] = 'SFU: Bur -> Van'
@@ -38,10 +40,12 @@ btn_dir.place(x=0, y=0)
 
 # ESTIMATE interval label
 # label_bur = tk.Label(root, text="", bg="#FFFFFF", width=5,height=5)
-label_bur = tk.Label(root, text="", bg="#FFFFFF", width=10,height=5)
+label_bur = tk.Label(root, text="", bg="#FFFFFF")
+label_van = tk.Label(root, text="", bg="#FFFFFF")
 # pack()
 # pack_forget()
-label_bur.place(x=272, y=155)
+# label_bur.place(x=272, y=160)
+# label_van.place(x=260, y=260)
 
 
 # BUTTON for calculate
@@ -51,26 +55,30 @@ def cal_estimate():
         # Eastbound W Hastings St @ Granville St
         soup = translink_sfu.get_data(51374)
         bus_lst = translink_sfu.time_to_leave(soup)
+        print(bus_lst)
         for bus in bus_lst:
             if bus['bus_num'] == 'R5':
+                label_bur.place(x=272, y=160)
                 time_left = bus['bus_time']
-                interval = f"[{time_left+40},{time_left+60}]"
+                interval = f"[{time_left+40},{time_left+60}] minutes"
                 label_bur.config(text = interval)
     else:
         # SFU Transportation Centre @ Bay 1
-        soup = translink_sfu.get_data(60015)
+        soup = translink_sfu.get_data(53096)
         bus_lst = translink_sfu.time_to_leave(soup)
+        print(bus_lst)
         for bus in bus_lst:
             if bus['bus_num'] == 'R5':
+                label_van.place(x=260, y=260)
                 time_left = bus['bus_time']
-                interval = f"[{time_left+40},{time_left+60}]"
-                label_bur.config(text = interval)
+                interval = f"[{time_left+40},{time_left+60}] minutes"
+                label_van.config(text = interval)
 
 
 
-btn_dir = tk.Button(root, text="CALCULATE", bg="#CC0633", fg="white",
+btn_cal = tk.Button(root, text="CALCULATE", bg="#CC0633", fg="white",
                 font=('helvetica', 9, 'bold'), command=cal_estimate)
-btn_dir.place(x=620, y=0)
+btn_cal.place(x=620, y=0)
 
 
 # REQUIRED
